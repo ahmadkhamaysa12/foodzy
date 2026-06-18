3b2426800e147f73d7033ec3ce0abb7844024ef3
ahmadkhamaysa12 <ahmadkhamaysa2023@gmail.com>
2026-06-18 17:51:37 +0300

    setup router

---
+++ src/routes.jsx
@@ -0,0 +1,60 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Checkout from "./pages/checkout/Checkout";
+import Faq from "./pages/faq/Faq";
+import About from "./pages/about/About";
+import Shop from "./pages/shop/Shop";
+import Product from "./pages/product/Product";
+import Cart from "./pages/cart/Cart";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+        {
+            index: true,
+            element: <Home />,
+        },
+        {
+            path: "login",
+            element: <Login />,
+        },
+        {
+            path: "register",
+            element: <Register />,
+        },
+        {
+            path: "checkout",
+            element: <Checkout />,
+        },
+        {
+            path: "faq",
+            element: <Faq />,
+        },
+        {
+            path: "about",
+            element: <About />,
+        },
+        {
+            path: "shop",
+            element: <Shop />,
+        },
+        {
+            path: "product",
+            element: <Product />,
+        },
+        {
+            path: "cart",
+            element: <Cart />,
+        }
+
+    ]
+
+  },
+]);
+export default router;
